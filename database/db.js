1372441e22ac3e646dc8109862c44dcfd458be55
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-06-17 22:16:24 +0300

    refactor

--- database/db.js
+++ database/db.js
@@ -1,16 +1,3 @@
-// import pg from "pg";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// export const db = new pg.Pool({
-//   user: process.env.DATABASE_USER,
-//   password: process.env.DATABASE_PASSWORD,
-//   host: process.env.DATABASE_HOST,
-//   port: process.env.DATABASE_PORT,
-//   database: process.env.DATABASE_NAME,
-// });
-
 import { PrismaClient } from "@prisma/client/extension";
 
 export const db = new PrismaClient();
