06b949f0362812ab30672286617f4780bddd9125
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-06-17 22:28:03 +0300

    feat: getBook method

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,7 +2,7 @@ import { db } from "../database/db.js";
 
 class BooksController {
   async addBook(req, res) {}
-  async getBooks(req, res) {
+  async getBooks(_, res) {
     try {
       const books = await db.books.findMany();
       res.status(200).json(books);
@@ -11,7 +11,16 @@ class BooksController {
       res.status(400).send(error.message);
     }
   }
-  async getBook(req, res) {}
+  async getBook(req, res) {
+    try {
+      const { id } = req.param;
+      const book = await db.books.findFirst({ id });
+      res.status(200).json(book);
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error.message);
+    }
+  }
   async updateBook(req, res) {}
   async deleteBook(req, res) {}
 }
