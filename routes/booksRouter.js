18c9c7e873a15e6bb61513a5e594596adcddd519
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-06-19 21:08:27 +0300

    refactor: validator

--- routes/booksRouter.js
+++ routes/booksRouter.js
@@ -1,25 +1,31 @@
 import { Router } from "express";
 import { booksController } from "../controllers/booksController.js";
 import { jwtMiddleware } from "../middlewares/jwtMiddleware.js";
-import { bookValidation, errorsValidation, idParamValidation } from "../middlewares/validations.js";
+import * as validator from "../middlewares/validations.js";
 
 export const booksRouter = new Router();
 
 booksRouter.get("/books", booksController.getBooks);
-booksRouter.get("/books/:id", idParamValidation, errorsValidation, booksController.getBook);
-booksRouter.post("/books", jwtMiddleware.validateAdmin, bookValidation, errorsValidation, booksController.addBook);
+booksRouter.get("/books/:id", validator.idParamValidation, validator.errorsValidation, booksController.getBook);
+booksRouter.post(
+  "/books",
+  jwtMiddleware.validateAdmin,
+  validator.bookValidation,
+  validator.errorsValidation,
+  booksController.addBook
+);
 booksRouter.put(
   "/books/:id",
   jwtMiddleware.validateAdmin,
-  bookValidation,
-  idParamValidation,
-  errorsValidation,
+  validator.bookValidation,
+  validator.idParamValidation,
+  validator.errorsValidation,
   booksController.updateBook
 );
 booksRouter.delete(
   "/books/:id",
   jwtMiddleware.validateAdmin,
-  idParamValidation,
-  errorsValidation,
+  validator.idParamValidation,
+  validator.errorsValidation,
   booksController.deleteBook
 );
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,31 +1,30 @@
 import { Router } from "express";
 import { usersController } from "../controllers/usersController.js";
 import { jwtMiddleware } from "../middlewares/jwtMiddleware.js";
-import {
-  errorsValidation,
-  idParamValidation,
-  loginValidation,
-  registerValidation,
-  roleValidation,
-  tokenValidation,
-} from "../middlewares/validations.js";
+import * as validator from "../middlewares/validations.js";
 
 export const usersRouter = new Router();
 
-usersRouter.post("/users/register", loginValidation, registerValidation, errorsValidation, usersController.register);
-usersRouter.post("/users/login", loginValidation, errorsValidation, usersController.login);
+usersRouter.post(
+  "/users/register",
+  validator.loginValidation,
+  validator.registerValidation,
+  validator.errorsValidation,
+  usersController.register
+);
+usersRouter.post("/users/login", validator.loginValidation, validator.errorsValidation, usersController.login);
 usersRouter.get("/users/me", jwtMiddleware.validate, usersController.getMe);
 usersRouter.get(
   "/users/register/validate/:token",
-  tokenValidation,
-  errorsValidation,
+  validator.tokenValidation,
+  validator.errorsValidation,
   usersController.validateRegisterToken
 );
 usersRouter.put(
   "/users/:id/role",
   jwtMiddleware.validateAdmin,
-  idParamValidation,
-  roleValidation,
-  errorsValidation,
+  validator.idParamValidation,
+  validator.roleValidation,
+  validator.errorsValidation,
   usersController.updateRole
 );
