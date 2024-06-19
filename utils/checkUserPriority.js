128fc85bdcedb1c072983fac8139465474a4331e
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-06-19 18:20:28 +0300

    feat: checkUserPriority

--- utils/checkUserPriority.js
+++ utils/checkUserPriority.js
@@ -1,7 +1,8 @@
 import { ADMIN } from "../constants/userRoles.js";
 
-export function checkUserPriority(user, priorities) {
-  if (!user?.user_role) return false;
+export function checkUserPriority(user, priority) {
+  if (!user) return false;
+  else if (user.user_role == null) return false;
   else if (user.user_role === ADMIN) return true;
-  else return false;
+  else return !!(user.user_role & priority);
 }
